9376ab8f184d260ae5b8e8cf1089cdad156132a2
vincent-tso <vincent.wailap.tso@gmail.com>
2025-01-24 03:39:22 +1100

    Updated models with new attributes

--- player.rb
+++ player.rb
@@ -1,8 +1,13 @@
 class Player
-    attr_accessor :name, :money
+    attr_accessor :name, :money, :position
 
     def initialize(name, money)
         @name = name
         @money = money
+        @position = 0
+    end
+
+    def move(spaces, board_size)
+        @position = (@position + spaces) % board_size # Resets to Go after final tile
     end
 end
--- property.rb
+++ property.rb
@@ -1,10 +1,15 @@
 class Property
-    attr_accessor :name. :price, :colour, :type
+    attr_accessor :name. :price, :colour, :type, :owner
 
     def initialize(name, price, colour, type)
         @name = name
         @price = price
         @colour = colour
         @type = type
+        @owner = nil
+    end
+
+    def available?
+        @owner.nil? # Check if Property has owner
     end
 end
