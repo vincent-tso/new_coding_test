dff168fba98f006cbf34344b90ba046b9e6b0d7d
vincent-tso <vincent.wailap.tso@gmail.com>
2025-01-24 03:18:04 +1100

    Added property model foundations:

---
+++ property.rb
@@ -0,0 +1,10 @@
+class Property
+    attr_accessor :name. :price, :colour, :type
+
+    def initialize(name, price, colour, type)
+        @name = name
+        @price = price
+        @colour = colour
+        @type = type
+    end
+end
